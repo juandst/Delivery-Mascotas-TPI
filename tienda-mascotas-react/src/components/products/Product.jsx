7e26409d0203f41581921d458b6c06b4e365ea53
ddewde <martinaguirre80@gmail.com>
2022-05-14 20:02:29 -0300

    complete_files

---
+++ tienda-mascotas-react/src/components/products/Product.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ProductItem from './ProductItem';
+import "./Products.css";
+import "./ProductItem.css";
+
+const Products = ({ products }) => {
+  
+    return (
+      <div>
+        <ProductItem
+          id={products[0].id}
+          img={products[0].img}
+          name={products[0].name}
+          price={products[0].price}
+          quality={products[0].quality}
+          action={products[0].action}
+        />
+        <ProductItem
+        id={products[1].id}
+        img={products[1].img}
+        name={products[1].name}
+        price={products[1].price}
+        quality={products[1].quality}
+        action={products[1].action}
+        />
+        <ProductItem
+          id={products[2].id}
+          img={products[2].img}
+          name={products[2].name}
+          price={products[2].price}
+          quality={products[2].quality}
+          action={products[2].action}
+        />
+      </div>
+    );
+}
+
+export default Products;
---
+++ tienda-mascotas-react/src/components/products/ProductItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Products from './Products.jsx';
+import './ProductItem.css';
+
+const ProductItem = ({ id, img, name, price, quality, action }) => {
+  return (
+    <div className='product-item-container'>
+        <h3>{id}</h3>
+        <h3>{img}</h3>
+        <h2>{name}</h2>    
+        <h3>{price}</h3>    
+        <h3>{quality}</h3>    
+        <h3>{action}</h3>    
+    </div>
+  )
+}
+
+export default ProductItem

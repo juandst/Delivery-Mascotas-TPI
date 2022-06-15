e2dea2fba0e875451a5f3b07f5afa4529597e1d7
juan-ds <juanods1@gmail.com>
2022-06-15 18:58:01 -0300

    Re-order components

--- tienda-mascotas-react/src/components/Menu/Menu.js
+++ tienda-mascotas-react/src/components/Menu/Menu.js
@@ -2,11 +2,15 @@ import OptionsMenu from "./OptionsMenu";
 
 const Menu = ({ options }) => {
   return (
-    <div>
-      {options.map((option, index) => (
-        <OptionsMenu key={index} opc={option} />
-      ))}
-    </div>
+    <nav>
+      <h2>Delivery Mascotas</h2>
+      <h3>Tienda online</h3>
+      <div>
+        {options.map((option, index) => (
+          <OptionsMenu key={index} opc={option} />
+        ))}
+      </div>
+    </nav>
   );
 };
 

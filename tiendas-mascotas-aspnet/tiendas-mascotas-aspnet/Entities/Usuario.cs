6d53bedb783378deb4ea95f46b10a2a747309b8d
juan-ds <juanods1@gmail.com>
2022-06-17 21:12:23 -0300

    Add Usuario entity

---
+++ tiendas-mascotas-aspnet/tiendas-mascotas-aspnet/Entities/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace tiendas_mascotas_aspnet.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }   
+        public string NombreDeUsuario { get; set; }
+    }
+}

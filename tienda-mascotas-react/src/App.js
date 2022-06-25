afe1d41cf43d2621bd08abe998de00481fcdfbec
juan-ds <juanods1@gmail.com>
2022-06-25 16:05:23 -0300

    Remove unecessary import and const "options"

--- tienda-mascotas-react/src/App.js
+++ tienda-mascotas-react/src/App.js
@@ -1,26 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
-import { useState } from "react";
 import Menu from "./components/Menu/Menu";
 import RoutesWeb from "./components/RoutesWeb/RoutesWeb";
+import GlobalState from "./context/GlobalState";
 import "./App.css";
 
 const App = () => {
-  const [options, setOptions] = useState([
-    {
-      path: "/login",
-      label: "Ingresar",
-    },
-    {
-      path: "/register",
-      label: "Registrarse",
-    },
-  ]);
-
   return (
-    <BrowserRouter>
-      <Menu options={options} />
-      <RoutesWeb />
-    </BrowserRouter>
+    <GlobalState>
+      <BrowserRouter>
+        <Menu />
+        <RoutesWeb />
+      </BrowserRouter>
+    </GlobalState>
   );
 };
 

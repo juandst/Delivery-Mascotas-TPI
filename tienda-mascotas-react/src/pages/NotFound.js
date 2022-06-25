53b109e8f95f02f7e635929934b701cdc8a74089
juan-ds <juanods1@gmail.com>
2022-06-25 19:24:47 -0300

    Add image to NotFound page

--- tienda-mascotas-react/src/pages/NotFound.js
+++ tienda-mascotas-react/src/pages/NotFound.js
@@ -6,6 +6,7 @@ const NotFound = () => (
     </Link>
     <h3>404 page not found</h3>
     <p>La página solicitada no existe</p>
+    <img src="https://cdn.discordapp.com/attachments/989026778741899287/990379379580358697/comprar-un-perro.jpg" alt="perrito"></img>
   </div>
 );
 

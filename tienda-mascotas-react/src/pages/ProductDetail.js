17756f2a9cb834b41604fb118ddf67155a5ce91e
juan-ds <juanods1@gmail.com>
2022-06-25 16:08:00 -0300

    Add context

--- tienda-mascotas-react/src/pages/ProductDetail.js
+++ tienda-mascotas-react/src/pages/ProductDetail.js
@@ -1,13 +1,15 @@
 import firebase from "../config/firebase";
+import NetContext from "../context/NetContext";
 import { Link, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
 
-export default function ProductDetail({ id }) {
+export default function ProductDetail() {
   const [product, setProduct] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  id = useParams().id;
+  const id = useParams().id;
 
   useEffect(() => {
     firebase.db
@@ -16,24 +18,32 @@ export default function ProductDetail({ id }) {
       .then((doc) => {
         setProduct(doc);
         setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
       });
   }, [id]);
 
   return (
-    <>
-      <Link to={"/"}>
-        <button>Volver</button>
-      </Link>
-      {error && <p>{error}</p>}
-      {isLoading && <div>Cargando datos...</div>}
-      {product && (
-        <div>
-          <h1>{product.data().name}</h1>
-          <p>{product.data().description}</p>
-          <p>$ {product.data().price}</p>
-          <img src={product.data().image} alt=""></img>
-        </div>
+    <NetContext.Consumer>
+      {(context) => (
+        (
+          <Link to={"/"}>
+            <button>Volver</button>
+          </Link>
+        ),
+        error && <p>{error}</p>,
+        isLoading && <div>Cargando datos...</div>,
+        product && (
+          <div>
+            <h1>{product.data().name}</h1>
+            <p>{product.data().description}</p>
+            <p>$ {product.data().price}</p>
+            <img src={product.data().image} alt=""></img>
+            {context.login && <Button variant="warning">COMPRAR</Button>}
+          </div>
+        )
       )}
-    </>
+    </NetContext.Consumer>
   );
 }

52e11dedd9c5f08b02f9633aa5c07c74d0f23869
juan-ds <juanods1@gmail.com>
2022-06-26 15:48:10 -0300

    Update import

--- tienda-mascotas-react/src/App.js
+++ tienda-mascotas-react/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import Menu from "./components/Menu/Menu";
-import RoutesWeb from "./components/RoutesWeb/RoutesWeb";
+import RoutesWeb from "./RoutesWeb/RoutesWeb";
 import GlobalState from "./context/GlobalState";
 import "./App.css";
 

ab6e2d241e301ba94b6208fbd4efa21eba8e3206
juan-ds <juanods1@gmail.com>
2022-06-27 19:30:35 -0300

    Firebase config

--- tienda-mascotas-react/src/config/firebase.js
+++ tienda-mascotas-react/src/config/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyD0Yjl7cyZ3QS3DkoGZnF3QGNkX0Adml6I",
+  authDomain: "tup-tp-react.firebaseapp.com",
+  projectId: "tup-tp-react",
+  storageBucket: "tup-tp-react.appspot.com",
+  messagingSenderId: "424279241654",
+  appId: "1:424279241654:web:12a8ed0f4b730a07e44717",
 };
 
 firebase.initializeApp(firebaseConfig);

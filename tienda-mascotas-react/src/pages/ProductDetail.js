d504ae3bec160bedd454c7669701d71e4fe575f9
juan-ds <juanods1@gmail.com>
2022-06-28 15:01:52 -0300

    Add component ProductDetailCard

--- tienda-mascotas-react/src/pages/ProductDetail.js
+++ tienda-mascotas-react/src/pages/ProductDetail.js
@@ -1,10 +1,8 @@
-import NetContext from "../context/NetContext";
 import Loading from "../components/forms/Loading";
 import { getProduct } from "../services/ProductServices";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Card, Button } from "react-bootstrap";
-import { useContext } from "react";
+import ProductDetailCard from "../components/products/ProductDetailCard";
 
 export default function ProductDetail() {
   const [product, setProduct] = useState("");
@@ -12,7 +10,6 @@ export default function ProductDetail() {
   const [error, setError] = useState(null);
 
   const id = useParams().id;
-  const context = useContext(NetContext);
 
   useEffect(() => {
     getProduct(id)
@@ -26,35 +23,10 @@ export default function ProductDetail() {
   }, [id]);
 
   return (
-    error && <p>{error}</p>,
-    isLoading && <Loading />,
-    product && (
-      <>
-        <Link to={"/"}>
-          <Button variant="warning" style={{ marginTop: "10px" }}>
-            Volver
-          </Button>
-        </Link>
-        <Card border="warning" style={{ width: "80%", margin: "auto", marginTop: "50px" }}>
-          <Card.Body>
-            <Card.Img
-              variant="top"
-              src={product.data().image}
-              style={{ maxWidth: "25%", height: "auto", display: "block", marginLeft: "auto", marginRight: "auto" }}
-            />
-            <Card.Title>{product.data().name}</Card.Title>
-            <Card.Text>{product.data().description}</Card.Text>
-            <Card.Footer>$ {product.data().price}</Card.Footer>
-            {(context.login && <Button variant="warning">COMPRAR</Button>) || (
-              <Link to={"/login"}>
-                <Button variant="warning" style={{ marginTop: "10px" }}>
-                  INICIAR SESIÓN
-                </Button>
-              </Link>
-            )}
-          </Card.Body>
-        </Card>
-      </>
-    )
+    <>
+      {error && { error }}
+      {isLoading && <Loading />}
+      {product && <ProductDetailCard product={product} />}
+    </>
   );
 }

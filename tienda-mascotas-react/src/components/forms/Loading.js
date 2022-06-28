ee6aff9f1b179b3bb470519958a21879e7510a49
juan-ds <juanods1@gmail.com>
2022-06-28 15:01:06 -0300

    Add loading spinner

--- tienda-mascotas-react/src/components/forms/Loading.js
+++ tienda-mascotas-react/src/components/forms/Loading.js
@@ -1,3 +1,12 @@
+import { Spinner } from "react-bootstrap";
+
 export default function Loading() {
-  return <>Cargando datos...</>;
+  return (
+    <div className="loading">
+      <Spinner animation="border" variant="warning" style={{ height: "100px", width: "100px" }} />
+      <p>
+        <strong>CARGANDO</strong>
+      </p>
+    </div>
+  );
 }

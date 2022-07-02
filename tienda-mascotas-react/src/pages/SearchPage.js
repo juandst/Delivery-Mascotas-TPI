c15d731dee6e9568c134e5321bb00a9d01ff6c1e
juan-ds <juanods1@gmail.com>
2022-07-02 01:32:06 -0300

    Funciona (?)

--- tienda-mascotas-react/src/pages/SearchPage.js
+++ tienda-mascotas-react/src/pages/SearchPage.js
@@ -1,23 +1,35 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import Products from "../components/products/Products";
+import ProductItem from "../components/products/ProductItem";
 import Loading from "../components/forms/Loading";
 import { getProducts } from "../services/ProductServices";
 
-export default function SearchPage() {
+const SearchPage = () => {
   const params = useParams();
   const [products, setProducts] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [filteredProducts, setFilteredProducts] = useState([]);
   useEffect(() => {
     getProducts()
       .then((query) => {
-        setProducts(query.docs);
+        for (let i = 0; i < query.docs.length; i++) {
+          if (query.docs[i].data().name.toLowerCase().includes(params.params)) {
+            setFilteredProducts((prev) => [...prev, query.docs[i].data()]);
+            console.log("FILTERED: " + filteredProducts);
+          }
+        }
+        console.log(query.docs[0].data().name);
+        setProducts(query);
         setIsLoading(false);
       })
       .catch((err) => setError(err));
-  }, []);
+  }, [params.params]);
+
+  const productsMapped = filteredProducts.map((product, index) => {
+    return <ProductItem key={index} id={product.id} {...product} />; //data() es por firebase
+  });
 
   return (
     <>
@@ -29,11 +41,17 @@ export default function SearchPage() {
             <strong>Buscando: </strong>
             {params.params}
           </p>
-          <Container className="product-item-container">
-            <Products productList={products} />
-          </Container>
+          <Container className="product-item-container">{productsMapped}</Container>
         </>
       )}
     </>
   );
-}
+};
+
+export default SearchPage;
+
+//query es una promesa
+//query.docs es un array de productos
+//query.docs[0].data() es un objeto con los datos del producto
+//query.docs[0].data().name es el nombre del producto
+//etc
